bc6b512b224a0864ee904a28740dcbcd86ff47c5
Dmitry Stepanov <dmitrij@stepanov.lv>
2009-09-17 01:28:50 +0300

    Added licensing info and prepared for push to GITHUB.

--- src/js/jquery/uploader/jquery.uploader.js
+++ src/js/jquery/uploader/jquery.uploader.js
@@ -2,7 +2,23 @@
   * Author: Dmitry Stepanov
   * E-Mail: dmitrij@stepanov.lv
   * URL: http://www.stepanov.lv
-  */
+  
+  jQuery UI tools - JavaScript library.
+  Copyright (C) 2009  Dmitry Stepanov <dmitrij@stepanov.lv>
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 if( typeof jQuery =='undefined')
   throw 'jQuery library is required';
